57e632265bc75c7cf65a0bb1f577d8b145d7e047
TED Vortex <ted.vortex@gmail.com>
2021-12-25 00:22:16 +0200

    build: try cleaner replit build

--- shell.nix
+++ shell.nix
@@ -13,6 +13,8 @@ in
     inherit NPM_CONFIG_PREFIX;
 
     shellHook = ''
+      npm set prefix "${NPM_CONFIG_PREFIX}"
       export PATH="${NPM_CONFIG_PREFIX}/bin:$PATH"
+      npm i -g npm@latest
     '';
   }
