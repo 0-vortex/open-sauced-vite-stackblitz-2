bfc4f262609ee2ec3038245df0477e90d41bb515
TED Vortex <ted.vortex@gmail.com>
2021-12-24 16:55:45 +0200

    feat: plug in conditionals on build and test providers again

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,31 @@
 import { defineConfig } from 'vite'
 import ViteReact from '@vitejs/plugin-react'
 import ViteHtml from 'vite-plugin-html'
+import ViteLegacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(({command, mode, ...rest }) => {
+  // figure out commands
+  const isBuild = command === 'build';
+
+  // figure out modes
+  const isDev = mode === "development";
+  const isProd = mode === "production";
+  const isReport = mode === "report";
+
+  // figure out custom build options
+  const isLegacy = process.env.VITE_LEGACY || false;
+  const isGitpodBuild = process.env.GITPOD_WORKSPACE_URL || false;
+
+  const build = {
+    outDir: "build",
+    assetsDir: "static",
+    sourcemap: !isDev,
+    rollupOptions: {},
+    manifest: false,
+  };
+
+  const plugins = [
     ViteReact({
       fastRefresh: process.env.NODE_ENV !== 'test',
       // Exclude storybook stories
@@ -31,5 +52,20 @@ export default defineConfig({
         },
       },
     })
-  ]
-})
+  ];
+
+  return {
+    base: "/",
+    mode,
+    plugins,
+    publicDir: "public",
+    server: {
+      port: 3000,
+      open: !isGitpodBuild,
+    },
+    build,
+    preview: {
+      port: 3000,
+    }
+  };
+});
